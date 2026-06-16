76c2e1eb8603845e11f0517eeef136264be16335
Lukas Girniukaitis <lukasgirn@gmail.com>
2026-06-16 21:03:40 +0300

    pakeista

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from models.python_kursas import PythonKursas
 
 kursas = Kursas("Geimingas", "Lukas", 20)
 pythonkursas = PythonKursas("Programavimas", "Tomas", 370)
+kursas2 = PythonKursas("Python", "Jonas", 290)
 
 kursas.destyti()
 pythonkursas.destyti()
+kursas2.destyti()
